0de3f5d7707b2eae47c4e46bd941e27ca7005436
Suranjan Kumar Verma <myidsuranjanverma@gmail.com>
2026-06-21 23:34:55 +0530

    bug fix: explicitly define ObjectMapper

--- src/main/java/com/translator/language_translator/LanguageTranslatorApplication.java
+++ src/main/java/com/translator/language_translator/LanguageTranslatorApplication.java
@@ -1,13 +1,27 @@
 package com.translator.language_translator;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LanguageTranslatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LanguageTranslatorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LanguageTranslatorApplication.class, args);
+    }
 
+    // This creates the RestTemplate bean required by TranslationService
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    // This creates the ObjectMapper bean required by SpeechService
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
